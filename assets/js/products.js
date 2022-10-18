d99f28b3338c41f4b9e10601f7cc6d5aab08f84f
PhamNguyenHuyHoang <pnhh28012002@gmail.com>
2022-10-19 00:28:54 +0700

    Upload

---
+++ assets/js/products.js
@@ -0,0 +1,216 @@
+var productArray = [{
+        productId: 10001,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-1.jpg',
+        name: 'Hoa mẫu đơn màu cam bông lẻ – hoa giả cao cấp',
+        price: 18000,
+        pricedel: 0,
+        description: 'Hoa mẫu đơn màu cam lẻ – hoa giả đẹp cao cấp decor trang trí phòng ngủ phòng khách – hoa giả chụp ảnh',
+        detailDescription: '<b>Tên sản phẩm: Hoa mẫu đơn màu cam bông lẻ </b><br> Mã sản phẩm: SP000591<br> Giá là của 1 bông. Như hình 9 bông <br> Vật liệu: hoa vải<br> Màu sắc : Như hình. <br> Sử dụng để: trang trí nhà, trang trí décor quán café, cắm hoa'
+    }
+
+    ,
+    {
+        productId: 10002,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-2.jpg',
+        name: 'Mô hình đồng hồ Big Bang sắt London Anh',
+        price: 178000,
+        pricedel: 300000,
+        description: 'Mô hình tháp đồng hồ Big Bang kim loại decor trang trí phòng tủ kệ bàn làm việc Tháp đồng hồ Big bang Anh phong cách vintage đẹp tối giản độc đáo',
+        detailDescription: 'Tên sản phẩm: Mô hình đồng hồ để bàn Big Bang sắt London Anh <br> Kích thước: 23.5 x 5 x 5 cm. <br>Màu: như hình<br> Chất liệu: thép<br>Tham khảo thêm các mẫu đồng hồ đẹp khác tại: <a href="https://greenholo.htcnw.kienlab.com/products.html">đây</a> '
+    }
+
+    ,
+    {
+        productId: 10003,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-3.jpg',
+        name: 'chậu cây xanh decor vintage',
+        price: 325000,
+        pricedel: 500000,
+        description: `<b>description sẽ hiện cạnh hình ảnh chi tiết sản phẩm.</b> <br> Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...`,
+        detailDescription: ` Tên sản phẩm: chậu cây xanh decor vintage. <br> Chất liệu: lá xanh nhựa+ chậu đất nung vẽ giả vintage. <br> Kích thước: nhiều size. chậu cây cao 10cm, 13cm, 26cm và cao 32cm. <br> Tham khảo thêm các mẫu hoa giả đẹp khác tại: <a href="https://greenholo.htcnw.kienlab.com/products.html">đây</a>`
+    }
+
+    , // feedmore-slick
+    {
+        productId: 10004,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-4.jpg',
+        name: 'Bảng gỗ thủy tinh treo tường đơn giản độc đáo trang trí',
+        price: 129000,
+        pricedel: 300000,
+        description: 'Bảng gỗ thủy tinh treo tường đơn giản độc đáo trang trí tường nhà quán cafe – không kèm hoa',
+        detailDescription: 'Tên sản phẩm: Bảng gỗ thủy tinh treo tường đơn giản độc đáo trang trí tường nhà quán cafe – không kèm hoa<br> Mã sản phẩm: trên hình.<br> Chất liệu: gỗ + bình hoa lọ thủy tinh.'
+    }
+
+    ,
+    {
+        productId: 10005,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-5.jpg',
+        name: 'Kệ Home 1 tầng treo tường trang trí',
+        price: 215000,
+        pricedel: 0,
+        description: 'Kệ Home 1 tầng treo tường trang trí – kệ gỗ decor trang trí phòng ngủ phòng khách quán cafe đẹp',
+        detailDescription: '<b>Tên sản phẩm:</b> Kệ Home 1 tầng. <br><b>Mã sản phẩm:</b> trên hình. <br><b>Chất liệu:</b> gỗ + sắt. <br><b>Màu:</b> xanh, xám, vàng và đỏ. <br></b><b>Sử dụng:</b> kệ gỗ treo tường, kệ decor, kệ trang trí, kệ gỗ trang trí phòng ngủ, kệ home, tủ kệ trang trí, bảng gỗ treo cửa, bảng gỗ trang trí, bảng gỗ treo cửa trang trí, đồ trang trí bàn, trang trí tủ kệ phòng khách, trang trí phòng ngủ, trang trí nhà, decor quán cafe đẹp, trang trí tường, trang trí spa, cửa hàng, tặng quà tân gia nhà mới, tặng khai trương phát tài, trang trí phong thủy…v….v <br>Xem thêm sản phẩm tại: <a href="https://greenholo.htcnw.kienlab.com/products.html">đây</a> '
+    }
+
+    ,
+    {
+        productId: 10006,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-6.jpg',
+        name: 'Móc vintage Home đỏ',
+        price: 129000,
+        pricedel: 300000,
+        description: 'Móc treo đồ phong cách vintage Home đỏ retro decor trang trí nhà đẹp <br>Móc treo quần áo túi xách chìa khóa đẹp decor trang trí phòng',
+        detailDescription: ' <b>Thông tin sản phẩm móc gỗ treo đồ decor trang trí:</b> <br> <b>Tên mẫu bảng gỗ treo:</b> Móc treo đồ phong cách vintage Home đỏ.<br><b>Chất liệu:</b> gỗ ép + móc sắt.<br> Dùng để treo tường trang trí, đây là sản phẩm bảng gỗ treo cửa handmade, xuất sứ Việt Nam.<br>Xem thêm sản phẩm tại: <a href="https://greenholo.htcnw.kienlab.com/products.html">đây</a>'
+    }
+
+    ,
+    {
+        productId: 10007,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-7.jpg',
+        name: 'Gương dán tường trang trí Mica hình lục giác màu bạc',
+        price: 25000,
+        pricedel: 0,
+        description: 'Tên sản phẩm: Gương dán tường trang trí mica hình lục giác màu bạc.',
+        detailDescription: '<b>Chất liệu:</b> mica giả gương (rất đẹp và sắt nét). <br><b>Kích thước:</b> size lớn ( 1 tấm có kích thước là 16 x 18 x 9cm). <br>Lẻ 1 miếng mã SP000199. <br>1 bộ 12 miếng mã SP001701. <br>Tham khảo thêm các mẫu gương dán tường tranh dán tường mica khác tại <a href="https://greenholo.htcnw.kienlab.com/products.html">đây</a>'
+    }
+
+    ,
+    {
+        productId: 10008,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-8.jpg',
+        name: 'Tranh treo tường xương rồng nghệ thuật',
+        price: 495000,
+        pricedel: 695000,
+        description: 'Sản phẩm Tranh treo tường canvas nghệ thuật lá cây kết hợp câu nói ý nghĩa về gia đình thích hợp để: trang trí phòng khách, trang trí phòng ngủ, trang trí bếp, làm quà tặng tân gia.',
+        detailDescription: '<b>Chất liệu:</b> tranh in trên vải canvas.</br> <b>Màu sắc:</b> chân thật, sắc nét.<br><b>Dày:</b> 9 mm.<br><b>Khung:</b> gỗ. <br>Bộ 3 tranh như hình. <br>Xem thêm sản phẩm tranh treo tường nghệ thuật đẹp tại <a href="https://greenholo.htcnw.kienlab.com/products.html">đây</a>'
+    }
+
+    ,
+    {
+        productId: 10009,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-9.jpg',
+        name: 'Đồng hồ đầu hươu hoa tài lộc',
+        price: 795000,
+        pricedel: 895000,
+        description: 'Đồng hồ treo tường nghệ thuật trang trí decor cao cấp hình đầu hươu hoa tài lộc may mắn',
+        detailDescription: '<b>Tên sản phẩm:</b> Đồng hồ treo tường đầu hươu hoa tài lộc <br> <b>Kích thước:</b> 58 x 74cm. (ngang x dài). Mặt đồng hồ 24cm.<br><b>Phong cách:</b> đồng hồ phong cách sang trọng. <br><b>Màu sắc:</b> như hình. <br><b>Chất liệu: </b>đầu hươu gỗ + sắt sơn tĩnh điện.'
+    },
+    {
+        productId: 10010,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-10.jpg',
+        name: 'Hộp khăn giấy pha lê sang trọng màu vàng đồng',
+        price: 379000,
+        pricedel: 0,
+        description: '',
+        detailDescription: ''
+    },
+    {
+        productId: 10011,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-11.jpg',
+        name: 'Thùng rác nhựa nắp bật màu hồng',
+        price: 135000,
+        pricedel: 0,
+        description: '',
+        detailDescription: '<b>Tên sản phẩm:</b> thùng đựng rác nắp bật.<br> <b>Chất liệu:</b> nhựa. <br><b>Kích thước:</b> cao 32 x 19cm. <br><b>Màu sắc:</b> hồng và xám. <br><b>Sử dụng:</b> thùng đựng rác, thùng đựng rác nhựa nắp bật, trang trí nhà bếp, thùng đựng rác nhà vệ sinh, thùng đựng rác wc, trang trí phòng ngủ, trang trí nhà, decor quán cafe đẹp,trang trí spa, cửa hàng, …v….v..'
+    },
+    {
+        productId: 10012,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-12.jpg',
+        name: 'Bộ 3 lông vũ trang trí',
+        price: 225000,
+        pricedel: 0,
+        description: '',
+        detailDescription: ''
+    },
+    {
+        productId: 10013,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-13.jpg',
+        name: 'Giỏ sắt lưới màu đen- màu trắng',
+        price: 139000,
+        pricedel: 200000,
+        description: 'Giỏ sắt lưới màu đen- màu trắng đựng đồ tiện dụng',
+        detailDescription: '<b>Tên sản phẩm:</b> giỏ sắt lưới đựng đồ.<br> <b>Chất liệu:</b> sắt.<br><b>Màu sắc:</b> màu đen, màu trắng. <br><b>Kích thước:</b> 21.8 x 28 x 15.8 (cao x dài x rộng). <b>Sử dụng:</b> đựng đồ, đựng bảng gỗ treo cửa, bảng gỗ trang trí, bảng gỗ treo cửa trang trí, đồ trang trí bàn, trang trí tủ kệ phòng khách, trang trí phòng ngủ, trang trí nhà, decor quán cafe đẹp, trang trí tường, trang trí spa, cửa hàng, tặng quà tân gia nhà mới, tặng khai trương phát tài, trang trí phong thủy…v….v.. <br>Xem thêm sản phẩm trang trí tại link: <a href="https://greenholo.htcnw.kienlab.com/products.html">đây</a>'
+    },
+    {
+        productId: 10014,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-14.jpg',
+        name: 'Móc câu treo trang trí',
+        price: 79000,
+        pricedel: 0,
+        description: 'Móc câu treo trang trí chủ đề biển phong cách địa trung hải',
+        detailDescription: ''
+    },
+    {
+        productId: 10015,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-15.jpg',
+        name: 'Hộp khăn giấy sắt container xanh',
+        price: 95000,
+        pricedel: 120000,
+        description: '',
+        detailDescription: ''
+    },
+    {
+        productId: 10016,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-16.jpg',
+        name: 'Hộp đựng giấy vệ sinh',
+        price: 55000,
+        pricedel: 0,
+        description: 'Hộp đựng giấy vệ sinh tiện lợi để trong nhà tắm – phòng wc dán tường chống thấm',
+        detailDescription: '<b>Tên sản phẩm:</b> Hộp đựng giấy vệ sinh tiện lợi để trong nhà tắm – phòng wc dán tường chống thấm. <br><b>Chất liệu: </b>nhựa. <br><b>Màu sắc:</b> xám và trắng. <br><b>Sử dụng:</b> trang trí nhà tắm, để nhà vệ sinh, đồ gia dụng, đựng giấy nhà vệ sinh , wc, trang trí nhà, decor quán cafe đẹp, trang trí tường, trang trí spa, cửa hàng, tặng quà tân gia nhà mới, tặng khai trương phát tài, trang trí phong thủy…v….v.. <br>Xem thêm sản phẩm trang trí tại link: <a href="https://greenholo.htcnw.kienlab.com/products.html">đây</a>'
+    }
+
+
+];
+
+function getProductIndex(productid) 
+{
+	return productArray.findIndex(e => e.productId == productid);
+}
+
+function getProductSlice(start, end) 
+{
+	return productArray.slice(start, end);
+}
+
+function trendingProductsRender()
+{
+
+	var html = ``;
+	getProductSlice(0,6).forEach(e => {
+		html += `
+			<li>
+				<div class="product-item">
+					<a href="detail-product.html?productid=${e.productId}" class="product-thumb">
+						<img src=${e.img} alt="">
+					</a>
+					<div class="product-info">
+						<a href="detail-product.html?productid=${e.productId}" class="product-name">
+							${e.name}</a>
+						<br>
+						<div class="product-price">${e.price} đ</div>
+						<del>${e.pricedel} đ</del>
+						<div class="detail">
+							<a href="detail-product.html?productid=${e.productId}">Chi tiết <span><i class="fas fa-angle-right"></i></span></a>
+						</div>
+					</div>
+				</div>
+			</li>`;
+	});
+	return html;
+}
